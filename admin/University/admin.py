9e6eafa4e600b9307f4c948ea579faadf511818e
superdef454 <superdef454@mail.ru>
2024-04-20 20:46:32 +0500

    +модельки и отображение

--- admin/University/admin.py
+++ admin/University/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from .models import City, University, Faculty, Department, Group, Student, LegalDocument, LegalDocumentAcceptance, Organization, Agent, Publication, Like
 
-# Register your models here.
+# Настройка для города
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Настройка для университета
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'city')
+    list_filter = ('city',)
+    search_fields = ('title',)
+
+# Настройка для факультета
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'university')
+    list_filter = ('university',)
+    search_fields = ('title',)
+
+# Настройка для кафедры
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'faculty')
+    list_filter = ('faculty',)
+    search_fields = ('title',)
+
+# Настройка для группы
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'course')
+    list_filter = ('department', 'course')
+    search_fields = ('title',)
+
+# Настройка для студента
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('uid_keycloak_user', 'group', 'city')
+    list_filter = ('group', 'city')
+    search_fields = ('uid_keycloak_user',)
+
+# Настройка для документа
+@admin.register(LegalDocument)
+class LegalDocumentAdmin(admin.ModelAdmin):
+    list_display = ('document_type', 'enabled', 'version')
+    list_filter = ('enabled',)
+    search_fields = ('document_type', 'version')
+
+# Настройка для согласования документа
+@admin.register(LegalDocumentAcceptance)
+class LegalDocumentAcceptanceAdmin(admin.ModelAdmin):
+    list_display = ('LegalDocument', 'accepted_at', 'uid_keycloak_user')
+    list_filter = ('accepted_at',)
+    search_fields = ('uid_keycloak_user',)
+
+# Настройка для организации
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Настройка для представителя
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('uid_keycloak_user', 'city', 'organization')
+    list_filter = ('city', 'organization')
+    search_fields = ('uid_keycloak_user',)
+
+# Настройка для публикации
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('uid_keycloak_user', 'datetime', 'anonymous')
+    list_filter = ('datetime', 'anonymous')
+    search_fields = ('uid_keycloak_user', 'description')
--- admin/University/models.py
+++ admin/University/models.py
@@ -1,3 +1,210 @@
 from django.db import models
 
-# Create your models here.
+
+# Модель города
+class City(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+# Модель университета
+class University(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='universities/')
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Университет'
+        verbose_name_plural = 'Университеты'
+
+
+# Модель факультета
+class Faculty(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Факультет'
+        verbose_name_plural = 'Факультеты'
+
+# Модель кафедры
+class Department(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Кафедрa'
+        verbose_name_plural = 'Кафедры'
+
+# Модель группы
+class Group(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    course = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'группа'
+        verbose_name_plural = 'группы'
+
+# Модель студента
+class Student(models.Model):
+    uid_keycloak_user = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='students/')
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+
+# Модель документа
+class LegalDocument(models.Model):
+    document_type = models.CharField(max_length=255)
+    enabled = models.BooleanField(default=True)
+    version = models.CharField(max_length=255)
+    word_file = models.FileField(upload_to='legal_documents/')
+
+    class Meta:
+        verbose_name = 'Документ'
+        verbose_name_plural = 'Документы'
+
+# Модель согласования документа
+class LegalDocumentAcceptance(models.Model):
+    LegalDocument = models.ForeignKey(LegalDocument, on_delete=models.CASCADE)
+    accepted_at = models.DateField()
+    uid_keycloak_user = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'согласования факта участия'
+        verbose_name_plural = 'согласования факта участия'
+
+# Модель организации (вуза)
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='organizations/')
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Вуз'
+        verbose_name_plural = 'Вузы'
+
+# Модель представителя
+class Agent(models.Model):
+    uid_keycloak_user = models.CharField(max_length=255)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='agents/')
+    post = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+# Модель публикации
+class Publication(models.Model):
+    uid_keycloak_user = models.CharField(max_length=255)
+    datetime = models.DateTimeField()
+    description = models.TextField()
+    anonymous = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'публикации'
+
+# Модель лайка
+class Like(models.Model):
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    id_keycloak_user = models.IntegerField()
+# university (Университет)
+# title: str
+# description: str null=true
+# image: image
+# images: image
+# city: f-key
+
+# faculty (факультет)
+# university: f-key
+# title: str
+# description null=true
+
+# department (кафедрa)
+# faculty: f-key
+# title: str
+# description: str null=true
+
+# group (группа)
+# department f-key
+# title: str
+# description: str null=true
+# course: int (курс)
+
+# city (город)
+# name: str
+
+
+# student (Студент)
+# uid_keycloak_user: str
+# group: f-key null=true
+# city: f-key
+# image: image
+# description null=true
+
+# LegalDocument (Документы)
+# document_type: str (authorisation, registration, ...)
+# enabled: bool (включен или нет)
+# version: str (версия документа)
+# word_file: file (сам файл документа для подписания)
+
+# LegalDocumentAcceptance (согласования факта участия в мероприятии)
+# LegalDocument: f-key
+# accepted_at: date
+# uid_keycloak_user: str
+
+
+# organization (вуз)
+# name: str
+# logo: image
+# description: str null=true
+
+# agent (представитель)
+# uid_keycloak_user: str
+# city: f-key
+# organization: f-key
+# image: image
+# post: str (должность)
+# description: str null=true
+
+
+# Publication
+# uid_keycloak_user: str (кто опубликовал) если не анонимно
+# datetime: datetime
+# description: str
+# anonymous: bool
+
+# like
+# Publication: f-key
+# id_keycloak_user: int
